c47182c5833f11fc75946c0997648d4c5ba4eb0c
patrikbeno <patrikbeno@gmail.com>
2016-05-02 07:11:13 +0200

    Update configure.sh

--- router/configure.sh
+++ router/configure.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "Installing components..."
 dnf install -y dnsmasq squid httpd haproxy
 
+echo "Copying configuration..."
 cp -rfv $basedir/etc /
+
+echo "Initializing squid cache..."
+squid -z
+
+systemctl enable dnsmasq squid
+systemctl start dnsmasq squid
