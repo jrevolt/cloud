73666cb6fcb1f9babf83f01058669c39644c9821
patrikbeno <patrikbeno@gmail.com>
2016-05-02 07:30:46 +0200

    Update configure.sh

--- template/configure.sh
+++ template/configure.sh
@@ -6,3 +6,12 @@ pip uninstall pyvmomi
 
 echo "Delegating to SaltStack Bootstrap..."
 curl -sL http://bootstrap.saltstack.com | sh
+
+echo "Removing eth1 interface (leaving single eth0/DHCP)..."
+rm -fv /etc/systemd/network/eth1.network
+
+echo "Prepare the system for cloning..."
+curl -sL -o /var/tmp/sysprep.sh https://raw.githubusercontent.com/ifireball/linux-sysprep/master/sysprep.sh
+/var/tmp/sysprep.sh -f
+sys-unconfig
+
