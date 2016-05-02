18ae3d669fd8f2b3ba2f9862fe1aa4cc47365fbd
patrikbeno <patrikbeno@gmail.com>
2016-05-02 07:43:41 +0200

    Update configure.sh

--- router/configure.sh
+++ router/configure.sh
@@ -6,6 +6,16 @@ dnf install -y dnsmasq squid httpd haproxy
 echo "Copying configuration..."
 cp -rfv $basedir/etc /
 
+echo "Configuring router firewall.."
+firewall-cmd --zone=internal --change-interface=eth0
+firewall-cmd --zone=external --change-interface=eth1
+firewall-cmd --zone=internal \
+    --add-service=ntp --add-service=dns --add-service=dhcp \
+    --add-service=squid \
+    --add-service=http --add-service=https
+firewall-cmd --zone=external --add-service=http --add-service=https
+firewall-cmd --runtime-to-permanent
+
 echo "Initializing squid cache..."
 squid -z
 
